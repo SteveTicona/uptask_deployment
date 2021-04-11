304729dd02909dc08eb188dee2f1705f52fb3550
Steve Ticona <swattycons@gmail.com>
2021-04-10 22:42:08 -0400

    Autenticacion via Gmail Agregada

--- config/email.js
+++ config/email.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: 'variables.env' });
+
 module.exports = {
-  user: 'e201edb2f9b41a',
-  pass: 'd8296e5ff0409d',
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  user: process.env.GMAILUSSER,
+  pass: process.env.GMAILPASS,
+  host: 'smtp.gmail.com',
+  port: 465,
 };
